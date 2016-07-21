23693f72d012dd62c439b65587c8845e42e9d43e
Student-003 <student-003@fexli-itss-macbook-pro.local>
2016-07-21 11:22:41 +0530

    Initial Commit

---
+++ StopWatch1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StopWatch1
+//
+//  Created by Student-003 on 21/07/16.
+//  Copyright © 2016 Felix-ITs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
